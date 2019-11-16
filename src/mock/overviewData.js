f291df4b22823de9b6c52029510fc35e806833ae
HoangLong <long@callstats.io>
2019-11-16 15:08:33 +0200

    sample data

---
+++ src/mock/overviewData.js
@@ -0,0 +1,26 @@
+export const oneMonthOverviewData = [
+    {
+      goal: "1000",
+      currentBalance: "1550",
+      income: "2650",
+      expense: "1300",
+    },
+]
+
+export const threeMonthOverviewData = [
+    {
+      goal: "3000",
+      currentBalance: "3550",
+      income: "2650",
+      expense: "2100",
+    },
+]
+
+export const oneYearOverviewData = [
+    {
+      goal: "12000",
+      currentBalance: "10550",
+      income: "2650",
+      expense: "3000",
+    },
+]
