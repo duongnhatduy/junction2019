2c9b4009c11b3e5d2272eeb7153f200e72d5609c
Duy Duong <duy.duong@smarp.com>
2019-11-16 19:19:51 +0200

    icons

--- src/feature/Overview/CostList.jsx
+++ src/feature/Overview/CostList.jsx
@@ -5,12 +5,46 @@ import {
   Card,
   Grid,
   Typography,
-  LinearProgress
+  LinearProgress,
+  makeStyles
 } from '@material-ui/core';
+import HomeOutlinedIcon from '@material-ui/icons/HomeOutlined';
+import DriveEtaOutlinedIcon from '@material-ui/icons/DriveEtaOutlined';
+import LocalGasStationOutlinedIcon from '@material-ui/icons/LocalGasStationOutlined';
+import EvStationOutlinedIcon from '@material-ui/icons/EvStationOutlined';
+import PhoneOutlinedIcon from '@material-ui/icons/PhoneOutlined';
+import LocalGroceryStoreOutlinedIcon from '@material-ui/icons/LocalGroceryStoreOutlined';
+import FastfoodOutlinedIcon from '@material-ui/icons/FastfoodOutlined';
+import TheatersOutlinedIcon from '@material-ui/icons/TheatersOutlined';
+import WbIncandescentOutlinedIcon from '@material-ui/icons/WbIncandescentOutlined';
+
+const useStyles = makeStyles(theme => ({
+  costName: {
+    textTransform: 'capitalize'
+  }
+}));
+
+const NAME_MAP = {
+  carBill: 'car bill',
+  eatingOut: 'eat out',
+  phoneBill: 'phone bill'
+};
+
+const ICON_MAP = {
+  rent: <HomeOutlinedIcon />,
+  carBill: <DriveEtaOutlinedIcon />,
+  gas: <LocalGasStationOutlinedIcon />,
+  electric: <EvStationOutlinedIcon />,
+  eatingOut: <FastfoodOutlinedIcon />,
+  groceries: <LocalGroceryStoreOutlinedIcon />,
+  entertainment: <TheatersOutlinedIcon />,
+  phoneBill: <PhoneOutlinedIcon />,
+  utilities: <WbIncandescentOutlinedIcon />
+};
 
 export const CostList = ({ series, totalExpense }) => {
-  console.log(totalExpense);
   const { fixCost, variableCost } = aggregateTotalCost(series);
+
   return (
     <Card>
       <GroupedList
@@ -27,54 +61,66 @@ export const CostList = ({ series, totalExpense }) => {
   );
 };
 
-const GroupedList = ({ header, costMap, totalExpense }) => (
-  <Container>
-    <Box p={1}>
-      <Typography variant="h5" component="h5">
-        {header}
-      </Typography>
-      {Object.entries(costMap).map(([key, value]) => {
-        const percentage = (value / totalExpense) * 100;
-        return (
-          <Box p={1} my={2}>
-            <Grid
-              container
-              direction="row"
-              justify="space-between"
-              alignItems="center"
-              key={key}
-            >
-              <Grid item xs={9}>
-                {key}
-              </Grid>
+const CategoryName = ({ name }) => (NAME_MAP[name] ? NAME_MAP[name] : name);
+const Icon = ({ name }) => (ICON_MAP[name] ? ICON_MAP[name] : null);
+const Category = ({ name }) => (
+  <Grid container direction="row" justify="flex-start" alignItems="center">
+    <Icon name={name} />
+    <CategoryName name={name} />
+  </Grid>
+);
+
+const GroupedList = ({ header, costMap, totalExpense }) => {
+  const classes = useStyles();
+  return (
+    <Container>
+      <Box p={1}>
+        <Typography variant="h5" component="h5">
+          {header}
+        </Typography>
+        {Object.entries(costMap).map(([key, value]) => {
+          const percentage = (value / totalExpense) * 100;
+          return (
+            <Box p={1} my={2}>
               <Grid
                 container
                 direction="row"
                 justify="space-between"
-                alignItems="flex-end"
-                item
-                xs={3}
+                alignItems="center"
+                key={key}
               >
-                <Typography display="inline" color="textSecondary">
-                  {`${percentage.toFixed(0)}%`}
-                </Typography>
-                <Typography display="inline" color="primary">
-                  {`${value}`}
-                </Typography>
-              </Grid>
+                <Grid item xs={9} className={classes.costName}>
+                  <Category name={key} />
+                </Grid>
+                <Grid
+                  container
+                  direction="row"
+                  justify="space-between"
+                  alignItems="flex-end"
+                  item
+                  xs={3}
+                >
+                  <Typography display="inline" color="textSecondary">
+                    {`${percentage.toFixed(0)}%`}
+                  </Typography>
+                  <Typography display="inline" color="textPrimary">
+                    {`${value}`}
+                  </Typography>
+                </Grid>
 
-              <Grid item xs={12}>
-                <Box mt={1}>
-                  <LinearProgress variant="determinate" value={percentage} />
-                </Box>
+                <Grid item xs={12}>
+                  <Box mt={1}>
+                    <LinearProgress variant="determinate" value={percentage} />
+                  </Box>
+                </Grid>
               </Grid>
-            </Grid>
-          </Box>
-        );
-      })}
-    </Box>
-  </Container>
-);
+            </Box>
+          );
+        })}
+      </Box>
+    </Container>
+  );
+};
 
 function aggregateTotalCost(series) {
   return series.reduce(
