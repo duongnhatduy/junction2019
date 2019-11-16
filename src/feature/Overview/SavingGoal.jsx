1dbb4fc6ee2203b46098e6d0d2ce4c78985cf14e
Binh Tran <binh@kodit.io>
2019-11-16 15:13:40 +0200

    init SavingGoal

---
+++ src/feature/Overview/SavingGoal.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+
+export const SavingGoal = ({ currentBalance, goal, income, expense }) => {
+  const progress = (currentBalance / goal) * 100;
+  return <CircularProgressbar value={progress} text={`${progress.toFixed(0)}%`} />;
+};
