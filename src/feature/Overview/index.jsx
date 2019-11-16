0331e96b7f7b748d9ddf4624899bdc12210c1295
Duy Duong <duy.duong@smarp.com>
2019-11-16 12:52:26 +0200

    filter, overview page

--- src/feature/Overview/index.jsx
+++ src/feature/Overview/index.jsx
@@ -1,5 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { InputLabel, Select, MenuItem } from "@material-ui/core";
+import { getOverview, getSeries } from "../../api";
 
 export const Overview = () => {
-  return <>DM Binh</>;
+  const [groupBy, setGroupBy] = useState("1month");
+
+  const [overview, setOverview] = useState(null);
+  const [series, setSeries] = useState(null);
+
+  useEffect(() => {
+    async function fetch() {
+      const overviewData = await getOverview(groupBy);
+      const seriesData = await getSeries(groupBy);
+      setOverview(overviewData);
+      setSeries(seriesData);
+    }
+    fetch();
+  }, [groupBy]);
+
+  return (
+    <>
+      <InputLabel id="label">Time range</InputLabel>
+      <Select
+        labelId="label"
+        id="select"
+        value={groupBy}
+        onChange={({ target: { value } }) => setGroupBy(value)}
+      >
+        <MenuItem value="1month">1 Month</MenuItem>
+        <MenuItem value="3month">3 Months</MenuItem>
+        <MenuItem value="1year">1 Year</MenuItem>
+      </Select>
+    </>
+  );
 };
