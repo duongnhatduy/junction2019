c57d0c5878d94dcc28d25eb8b9209d7c8c2a5dae
HoangLong <long@callstats.io>
2019-11-16 21:09:21 +0200

    fix data

--- src/feature/PurchasePlan/purchase_predict.js
+++ src/feature/PurchasePlan/purchase_predict.js
@@ -1,31 +1,20 @@
+import { seriesData } from '../../mock/seriesData';
 import { sum } from '../../utils';
+import produce from 'immer';
 
-const currentMonthIndex = 11;
+// const currentMonthIndex = 4;
 
 export const purchasePredict = (fixCost, variableCost) => {
-  const seriData = {
-    month: 0,
-    income: 2650,
-    fixCost: {
-      rent: 508,
-      carBill: 250,
-      gas: 80,
-      electric: 50,
-      phoneBill: 20
-    },
-    variableCost: {},
-  };
-  const seriesData = []
-  let i = 0;
-  while (i < 12) {
-    const seri = JSON.parse(JSON.stringify(seriData))
-    seri.month = (i + 11) % 12
-    if (i === 0) {
-      seri.variableCost.extraCost = parseFloat(variableCost.down_payment);
-    }
-    seri.fixCost.extraCost = sum(Object.values(fixCost));
-    seriesData.push(seri)
-    i++
-  }
-  return seriesData;
+  return produce(seriesData, draft => {
+    draft.map((seri, i) => {
+      if (i === 0) {
+        seri.variableCost.extraCost = parseFloat(variableCost.down_payment);
+      }
+      if (i >= 0) {
+        seri.fixCost.extraCost = sum(Object.values(fixCost));
+      }
+      seri.month = (seri.month + 11) % 11
+      return seri;
+    });
+  });
 };
